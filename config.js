d15001316975b3b40e471826717c31addfa08efc
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-15 02:03:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -304,7 +304,7 @@ var config = {
                 //curve: 0, // change the speed at which it zooms out
             },
              mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
